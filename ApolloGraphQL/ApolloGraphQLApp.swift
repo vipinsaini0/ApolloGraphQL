ec7f6f611a69211b7c00b419a5224367b71e7a65
Vipin Saini <vipinsaini0@users.noreply.github.com>
2022-04-29 11:06:19 +0530

    Initial Commit

---
+++ ApolloGraphQL/ApolloGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloGraphQLApp.swift
+//  ApolloGraphQL
+//
+//  Created by Vipin Saini on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ApolloGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
